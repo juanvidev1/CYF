32a6d8185d0f7f3ae6fae30bb850e9d8e6927aaa
JuanviDev <juanvidev1@gmail.com>
2022-11-17 17:48:05 -0500

    Semicolon correction on CaughtPokemon

--- projects/react/pokedex/src/CaughtPokemon.js
+++ projects/react/pokedex/src/CaughtPokemon.js
@@ -1,6 +1,6 @@
 const CaughtPokemon = (props) => {
     //const date = {new Date().toLocaleDateString()}
-    console.log(props)
+    console.log(props);
     return(
       <p>Caught 0 pokemon on {props.date}</p>
     );
